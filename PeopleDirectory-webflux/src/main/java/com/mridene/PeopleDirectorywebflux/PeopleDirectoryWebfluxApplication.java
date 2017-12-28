214ea08de6b3af230cb6fccbe94382bb80a31f12
mridene <mridene@kapit.fr>
2017-12-28 14:40:47 +0100

    add spring boot and mongodb config

---
+++ PeopleDirectory-webflux/src/main/java/com/mridene/PeopleDirectorywebflux/PeopleDirectoryWebfluxApplication.java
@@ -0,0 +1,12 @@
+package com.mridene.PeopleDirectorywebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleDirectoryWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleDirectoryWebfluxApplication.class, args);
+	}
+}
---
+++ PeopleDirectory-webflux/src/test/java/com/mridene/PeopleDirectorywebflux/PeopleDirectoryWebfluxApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mridene.PeopleDirectorywebflux;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PeopleDirectoryWebfluxApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
